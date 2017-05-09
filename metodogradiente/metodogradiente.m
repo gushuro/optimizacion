af351c5abf6065fb8c57426aba1113c2c7d54c77
gushuro <gushurovich@gmail.com>
2017-05-09 18:46:15 -0300

    modificamos triseccion

--- metodogradiente/metodogradiente.m
+++ metodogradiente/metodogradiente.m
@@ -1,5 +1,5 @@
 function metodogradiente = metodogradiente(f, x0,y0)
-    %f = @(x,y) (x-y).^4 + 2*x.^2 + y.^2 - x + 2*y;
+    f = @(x,y) (x-y).^4 + 2*x.^2 + y.^2 - x + 2*y;
     N = 1000;
     x = zeros(N+1,1);
     y = zeros(N+1,1);
@@ -13,7 +13,8 @@ function metodogradiente = metodogradiente(f, x0,y0)
         if (norm(d) < 0.001)
             break;
         end
-        T = fminsearch(@(t) f(x(i)+t*d(1),y(i)+t*d(2)), 0);
+        %T = fminsearch(@(t) f(x(i)+t*d(1),y(i)+t*d(2)), 0);
+        T = triseccion(0, 80000, f, [x(i), y(i)], d);  
         x(i+1) = x(i)-T*g(1);
         y(i+1) = y(i)-T*g(2);
     end
