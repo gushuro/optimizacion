ee98dccb72edaec130acac2b456af49f36b6cec4
gushuro <gushurovich@gmail.com>
2017-05-09 18:37:15 -0300

    agrego metodo gradiente

---
+++ triseccion.m
@@ -0,0 +1,25 @@
+function triseccion = triseccion(a0,b0,f, xk, dk)
+    a = a0;
+    b = b0;
+    tol = 0.001;
+    
+    while norm(b-a) > tol
+        b
+        a
+        x1 = a + (b-a)/3;
+        x2 = a + (b-a)*2/3;
+        phix1 = f(xk + x1*dk);
+        phix2 = f(xk + x2*dk);
+        
+        if phix1 < phix2 -tol
+            b = x2;
+        elseif phix1 > phix2 + tol
+            a = x1;
+        else
+            a = x1;
+            b = x2;
+        end
+    end
+    triseccion = (a+b)/2; 
+   
+end
