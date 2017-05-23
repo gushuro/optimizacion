1cc759c5019c59f5836061b19e90051f982a7550
Agustina Florencia Lopez <aflopez@ws10.labo2>
2017-05-23 12:41:44 -0300

    modifico armijo, no anda

--- tp2/armijo.m
+++ tp2/armijo.m
@@ -1,22 +1,21 @@
 function armijo = armijo(f,x0,alpha,theta,gradiente)
 
-% NO LA LLEGUE A PROBAR
+    % NO LA LLEGUE A PROBAR
 
 
-% gradiente es el gradiente si es dado, pero vale 0 si no lo dan.
-x = x0;
-%n = size(x0,2);
-if gradiente == 0                    % si no me dan el gradiente, calcularlo numericamente
-        gradiente = gradNum(f,x);
-end
-for i = 1:tolIter
-    d = (-theta)*gradiente;   % elijo d asi para que cumpla lo pedido
-    lambda = 1;
-    while f(x + lambda*d) > f(x) + lambda*alpha*gradiente'*d
-        lambda = 0,5*lambda
+    % gradiente es el gradiente si es dado, pero vale 0 si no lo dan.
+    x = x0;
+    %n = size(x0,2);
+
+    for i = 1:1000 %cambiar por MaxNumIter
+        gradEnx = gradiente(x); 
+        d = (-theta)*gradEnx;   % elijo d asi para que cumpla lo pedido
+        lambda = 1;
+        while f(x + lambda*d) > f(x) + lambda*alpha*gradEnx'*d
+            lambda = 0.5*lambda;
+        end
+        x = x + lambda*d;
+
     end
-    x = x + lambda*d
-    gradiente = gradNum(f,x);
-end
-   armijo = x 
+       armijo = x 
 end
