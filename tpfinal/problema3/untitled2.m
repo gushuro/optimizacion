77c3527a8088b91573705594bdae8de33701dd4d
gus <gushurovich@gmail.com>
2018-09-14 16:33:02 -0300

    avances 14 septiembre

--- tpfinal/problema3/untitled2.m
+++ tpfinal/problema3/untitled2.m
@@ -1,20 +1,36 @@
-% I = imread('coins.png');
-% I = imread('foto.jpg');
-% I= imread('digits.jpeg');
-I= imread('wrenches.jpg');
-I = rgb2gray(I);
-% imshow(I)
+I = imread('coins.png');
+I = imread('foto.jpg');
+I= imread('digits.jpeg');
+% I= imread('wrenches.jpg');
+% I = imread('morron.png');
+I= imread('muscle2z-gray.png');
+% I = rgb2gray(I);
+imshow(I)
 %%
 BW1 = edge(I,'sobel');
 BW2 = edge(I,'canny');
 figure;
 imshowpair(BW1,BW2,'montage')
 title('Sobel Filter                                   Canny Filter');
-
-
+%%
+[BW, threshold] = edge(I, 'canny');
+threshold
+fudgeFactor = 1.5;
+BWs = edge(I,'canny', threshold * fudgeFactor);
+figure, imshow(BWs), title('binary gradient mask');
+%%
+se90 = strel('line', 3, 90);
+se0 = strel('line', 3, 0);
+BWsdil = imdilate(BWs, [se90 se0]);
+figure, imshow(BWsdil), title('dilated gradient mask');
+BWdfill = imfill(BWsdil, 'holes');
+figure, imshow(BWdfill);
+title('binary image with filled holes');
+BWnobord = imclearborder(BWdfill, 4);
+figure, imshow(BWnobord), title('cleared border image');
 %%
 A = zeros(size(I));
-tol=10;
+tol=20;
 for i=2:size(A,1)-1
     for j=2:size(A,2)-1
         centro=I(i,j);
@@ -32,6 +48,7 @@ for i=2:size(A,1)-1
     end
 end
 B=A;
+difuminados=0;
 for i=2:size(A,1)-1
     for j=2:size(A,2)-1
         centro=A(i,j);
@@ -43,16 +60,47 @@ for i=2:size(A,1)-1
         abajoizq = A(i+1,j-1);
         arribader = A(i-1,j+1);
         abajoder = A(i+1,j+1);
+        vecinos_blancos = (izquierda + derecha + arriba + abajo + arribaizq + arribader + abajoizq + abajoder)/255;
         if centro == 255
-            if izquierda + derecha + arriba + abajo + arribaizq + arribader + abajoizq + abajoder<=255
-               A(i,j) = 0; 
+            if vecinos_blancos <= 0
+                B(i,j) = 0; 
             end
+        elseif vecinos_blancos >=2
+            difuminados = difuminados + 1;
+%             B(i,j) = 255;
         end
     end
 end
-BW2 = edge(I,'canny');
+difuminados_2 = 0;
+% C=B;
+% for i=2:size(C,1)-1
+%     for j=2:size(C,2)-1
+%         centro=C(i,j);
+%         izquierda = C(i,j-1);
+%         derecha = C(i,j+1);
+%         arriba = C(i-1, j);
+%         abajo = C(i+1, j);
+%         arribaizq = C(i-1,j-1);
+%         abajoizq = C(i+1,j-1);
+%         arribader = C(i-1,j+1);
+%         abajoder = C(i+1,j+1);
+%         vecinos_blancos = (izquierda + derecha + arriba + abajo + arribaizq + arribader + abajoizq + abajoder)/255;
+%         if centro == 255
+%             if vecinos_blancos == 0
+%                 B(i,j) = 0; 
+%             end
+%         elseif vecinos_blancos >=2
+%             difuminados_2 = difuminados_2 + 1;
+%             B(i,j) = 255;
+%         end
+%     end
+% end
+difuminados
+difuminados_2
+BW2 = edge(B,'sobel');
 
 figure;
-imshowpair(A,BW2, 'montage');
+imshow(B);
+% imshowpair(B, BW2, 'montage');
 % montage(A);
             
